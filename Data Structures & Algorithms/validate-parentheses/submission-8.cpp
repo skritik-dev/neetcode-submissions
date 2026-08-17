acb26fa5dc654908b76330a63bd68d1d940f8954
skritik-dev <skritik0710@gmail.com>
2026-08-17 17:52:02 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+        for(const char& ch : str) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                s.push(ch);
+            } else {
+                if(!s.empty() && ((ch == ')' && s.top() == '(') || (ch == '}' && s.top() == '{') || (ch == ']' && s.top() == '['))) {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return s.empty();
+    }
+};
