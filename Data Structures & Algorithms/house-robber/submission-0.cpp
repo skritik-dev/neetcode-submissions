4dc91158b382a2cf8cd69fac5089d9682774b7aa
skritik-dev <skritik0710@gmail.com>
2026-08-25 12:08:06 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> dp(n + 2, 0);
+
+        for(int i = n - 1; i >= 0; i--) {
+            dp[i] = max(nums[i] + dp[i + 2], dp[i + 1]);
+        }
+
+        return dp[0];
+    }
+};
